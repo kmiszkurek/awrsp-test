75df3df32c240e1761597a31252b5cfe9e1aa951
kmiszkurek <konrad.miszkurek5@gmail.com>
2024-01-04 18:17:10 +0100

    changes in Second.java

--- src/Second.java
+++ src/Second.java
@@ -1,2 +1,16 @@
 public class Second {
+
+    public static void main(String[] args) {
+        
+    }
+
+    public static void main2(String[] args) {
+        for (int i = 0; i < ; i++) {
+            
+        }
+
+        for (int i = 0; i < ; i++) {
+            
+        }
+    }
 }
