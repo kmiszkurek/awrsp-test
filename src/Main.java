d29c6ed22fbe0b0ac1badd02a2c92ef9e92ac8fc
kmiszkurek <konrad.miszkurek5@gmail.com>
2024-01-04 18:24:10 +0100

    changes in Main.java

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,8 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("dziala");
+        System.out.println("dziala2");
+        System.out.println("dziala3");
+        System.out.println("dziala4");
     }
 }
